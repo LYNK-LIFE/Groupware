03556583da104b19a82feac722178a8d6a6afc9d
rlaskarb <rlaskarb20@gmail.com>
2024-12-04 14:47:05 +0900

    12/4 링크 남규 첫 커밋

--- src/main/java/com/semi/lynk/employee/controller/MainController.java
+++ src/main/java/com/semi/lynk/employee/controller/MainController.java
@@ -3,7 +3,7 @@ package com.semi.lynk.employee.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller//ps 안녕하세요 반갑습니다.
 public class MainController {
 
     @GetMapping("list")
