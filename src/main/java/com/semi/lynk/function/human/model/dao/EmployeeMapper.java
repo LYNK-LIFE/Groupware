ccf183daf4f0c0abb1820c3f3c7507dc9b555563
DESKTOP-GYUNAM <kimkunam06@gmail.com>
2024-12-08 15:19:16 +0900

    :wrench: 현재 분기 dev → main 병합 (현재 버전으로 반영)

--- src/main/java/com/semi/lynk/function/human/model/dao/EmployeeMapper.java
+++ src/main/java/com/semi/lynk/function/human/model/dao/EmployeeMapper.java
@@ -2,8 +2,6 @@ package com.semi.lynk.function.human.model.dao;
 
 import com.semi.lynk.function.human.model.dto.EmpAndDepDTO;
 import com.semi.lynk.function.human.model.dto.EmployeeDTO;
-import com.semi.lynk.function.human.model.dto.HumanDTO;
-import com.semi.lynk.function.human.model.dto.RegistDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -15,7 +13,12 @@ public interface EmployeeMapper {
 
     List<EmpAndDepDTO> joinListResult();
 
-    int registEmployee(Map<String, Object> map1);
+    int registMapperEmp(Map<String, Object> map1);
 
-    int registHuman(Map<String, Object> map2);
+    int registMapperhum(Map<String, Object> map2);
+
+//    int registEmployee(Map<String, Object> map1);
+////  int registHuman(Map<String, Object> map2);
+
+
 }
--- src/main/java/com/semi/lynk/function/human/service/EmployeeService.java
+++ src/main/java/com/semi/lynk/function/human/service/EmployeeService.java
@@ -1,10 +1,7 @@
 package com.semi.lynk.function.human.service;
 
 import com.semi.lynk.function.human.model.dao.EmployeeMapper;
-import com.semi.lynk.function.human.model.dto.EmpAndDepDTO;
-import com.semi.lynk.function.human.model.dto.EmployeeDTO;
-import com.semi.lynk.function.human.model.dto.HumanDTO;
-import com.semi.lynk.function.human.model.dto.RegistDTO;
+import com.semi.lynk.function.human.model.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,72 +29,57 @@ public class EmployeeService {
     }
 
     // DML용
-    @Transactional // 정상 처리 : commit , 예외 발생 : rollback
-    public Map<String, Integer> humanResister(RegistDTO registDTO) {
-        System.out.println("서비스 값 확인 registDTO" + registDTO);
-//        int id = registDTO.getId();
-//        String name = registDTO.getName();
-//        String address = registDTO.getAddress();
-//        String email = registDTO.getEmail();
-//        String position = registDTO.getPosition();
-//        String phoneNumber = registDTO.getPhoneNumber();
-//        String picture = registDTO.getPicture();
-////        String extensionNumber = registDTO.getExtensionNumber();
-////        String depName = registDTO.getDepName();
+//    @Transactional // 정상 처리 : commit , 예외 발생 : rollback
+//    public Map<String, Integer> humanResister(RegistEmpDTO registDTO) {
+//        System.out.println("서비스 값 확인 registDTO" + registDTO);
 //
-//        EmployeeDTO employeeDTO = new EmployeeDTO();
-//        employeeDTO.setId(id);
-//        employeeDTO.setName(name);
-//        employeeDTO.setEmail(email);
-//        employeeDTO.setPicture(picture);
+//        EmployeeDTO employeeDTO = EmployeeDTO.builder()
+//                .id(registDTO.getId())
+//                .name(registDTO.getName())
+//                .email(registDTO.getEmail())
+//                .picture(registDTO.getPicture())
+//                .build();
+//        System.out.println("employeeDTO" + employeeDTO);
+//        HumanDTO humanDTO = HumanDTO.builder()
+//                .position(registDTO.getPosition())
+//                .phoneNumber(registDTO.getPhoneNumber())
+//                .address(registDTO.getAddress())
+//                .build();
+//        System.out.println("humanDTO" + humanDTO);
 //
-//        HumanDTO humanDTO = new HumanDTO();
-//        humanDTO.setAddress(address);
-//        humanDTO.setPosition(position);
-//        humanDTO.setPhoneNumber(phoneNumber);
-
-        EmployeeDTO employeeDTO = EmployeeDTO.builder()
-                .id(registDTO.getId())
-                .name(registDTO.getName())
-                .email(registDTO.getEmail())
-                .picture(registDTO.getPicture())
-                .build();
-        System.out.println("employeeDTO" + employeeDTO);
-        HumanDTO humanDTO = HumanDTO.builder()
-                .position(registDTO.getPosition())
-                .phoneNumber(registDTO.getPhoneNumber())
-                .address(registDTO.getAddress())
-                .build();
-        System.out.println("humanDTO" + humanDTO);
-
-//        logic(employeeDTO , humanDTO);
-
-        Map <String, Object> map1 = new HashMap<>();
-        Map <String, Object> map2 = new HashMap<>();
-
-        map1.put("data1" , employeeDTO);
-        map2.put("data2", humanDTO);
-
-        int result1 = mapper.registEmployee(map1);
-        int result2 = mapper.registHuman(map2);
-        System.out.println("서비스 값 확인 result1" + result1);
-        System.out.println("서비스 값 확인 result2" + result2);
-
-        Map<String , Integer> result = new HashMap<>();
-        result.put("firstResult" , result1);
-        result.put("secondResult" , result2);
-        System.out.println("서비스 값 확인 result" + result);
-        return result;
-    }
-//    public void logic (EmployeeDTO emp , HumanDTO human) {
-//        emp.getId();
-//        emp.getName();
-//        emp.getEmail();
-//        emp.getPicture();
+////        logic(employeeDTO , humanDTO);
 //
-//        human.getAddress();
-//        human.getPosition();
-//        human.getPhoneNumber();
+//        Map <String, Object> map1 = new HashMap<>();
+//        Map <String, Object> map2 = new HashMap<>();
+//
+//        map1.put("data1" , employeeDTO);
+//        map2.put("data2", humanDTO);
+//
+//        int result1 = mapper.registEmployee(map1);
+//        int result2 = mapper.registHuman(map2);
+//        System.out.println("서비스 값 확인 result1" + result1);
+//        System.out.println("서비스 값 확인 result2" + result2);
+//
+//        Map<String , Integer> result = new HashMap<>();
+//        result.put("firstResult" , result1);
+//        result.put("secondResult" , result2);
+//        System.out.println("서비스 값 확인 result" + result);
+//        return result;
 //    }
+
+    @Transactional
+    public int humanRegist (RegistEmpDTO registEmpDTO
+                          , RegistHumDTO registHumDTO) {
+
+        Map<String, Object> map1 = new HashMap<>();
+        Map<String, Object> map2 = new HashMap<>();
+        map1.put("emp" , registEmpDTO);
+        map2.put("hum" , registHumDTO);
+        int result1 = mapper.registMapperEmp(map1);
+        int result2 = mapper.registMapperhum(map2);
+
+        return result1 >= 1 && result2 >= 1 ? 1 : 0;
+//        return result2;
+    }
 
 }
