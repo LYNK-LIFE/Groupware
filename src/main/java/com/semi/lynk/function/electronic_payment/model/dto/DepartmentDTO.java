b9ef498d318d43a21a25cdae434e56148edcb46f
rlaskarb <rlaskarb20@gmail.com>
2024-12-12 11:02:16 +0900

    12/12 메인 홈 화면 버튼명 수정 / 기안문서 -> 일반품의서 변경중

---
+++ src/main/java/com/semi/lynk/function/electronic_payment/model/dto/DepartmentDTO.java
@@ -0,0 +1,13 @@
+package com.semi.lynk.function.electronic_payment.model.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class DepartmentDTO {
+    private int departmentNo; //부서번호
+    private String departmentName; // 부서명
+}
