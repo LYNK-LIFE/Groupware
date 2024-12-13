5b2f99f7abdf8df139be6e64d653fbbb2a41771a
rlaskarb <rlaskarb20@gmail.com>
2024-12-13 19:01:48 +0900

    사용자 토글 생성 및 디자인, 관리자 페이지 폴더 구성

---
+++ src/main/java/com/semi/lynk/function/management/controller/managedController.java
@@ -0,0 +1,22 @@
+package com.semi.lynk.function.management.controller;
+
+import com.semi.lynk.function.management.service.ManagedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/function/management/*")
+public class managedController {
+
+    //****************************************************************
+    // 관리자 페이지 진입
+    //****************************************************************
+    @Autowired
+    private ManagedService managedService;
+
+    @GetMapping("activeAccountList")
+    public void activeAccountListPage() {}
+
+}
---
+++ src/main/java/com/semi/lynk/function/management/model/dao/ManagedMapper.java
@@ -0,0 +1,7 @@
+package com.semi.lynk.function.management.model.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ManagedMapper {
+}
