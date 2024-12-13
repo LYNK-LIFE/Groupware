f1e49cc33a618f333d7ae894bb7f2da4d05a06c9
Apexape <apexape84@gmail.com>
2024-12-13 17:51:54 +0900

    dto완료 dev변경 요청후 연결 완료되지 않음

--- src/main/java/com/semi/lynk/function/notice_board/model/dto/NoticeDTO.java
+++ src/main/java/com/semi/lynk/function/notice_board/model/dto/NoticeDTO.java
@@ -1,4 +1,21 @@
 package com.semi.lynk.function.notice_board.model.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class NoticeDTO {
+    private Long noticeNo;
+    private LocalDateTime noticeDate;
+    private int noticeVote;
+    private String noticeTitle;
+    private String noticeContent;
+    private int noticeHide;
+    private int viewerCount;
+    private String employeeNo;
 }
--- src/main/java/com/semi/lynk/function/notice_board/service/NoticeService.java
+++ src/main/java/com/semi/lynk/function/notice_board/service/NoticeService.java
@@ -1,4 +1,20 @@
 package com.semi.lynk.function.notice_board.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NoticeService {
+
+    @Autowired
+    private NoticeRepository noticeRepository;
+
+    public void saveNotice(NoticeDTO noticeDTO) {
+        Notice notice = convertToEntity(noticeDTO);
+        noticeRepository.save(notice);
+    }
+
+    private Notice convertToEntity(NoticeDTO noticeDTO) {
+        // DTO를 엔티티로 변환하는 로직
+        // ...
+    }
 }
