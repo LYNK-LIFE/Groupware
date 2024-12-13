fc487400251fed4ebc7446a359ddc935cb06dfc9
rlaskarb <rlaskarb20@gmail.com>
2024-12-13 18:18:57 +0900

    12/13 일반품의서 60% 완료

--- src/main/java/com/semi/lynk/function/electronic_payment/service/PaymentService.java
+++ src/main/java/com/semi/lynk/function/electronic_payment/service/PaymentService.java
@@ -1,8 +1,7 @@
 package com.semi.lynk.function.electronic_payment.service;
 
 import com.semi.lynk.function.electronic_payment.model.dao.PaymentMapper;
-import com.semi.lynk.function.electronic_payment.model.dto.ApproveDTO;
-import com.semi.lynk.function.electronic_payment.model.dto.EmployeeAndEctJoinDTO;
+import com.semi.lynk.function.electronic_payment.model.dto.DepAndEmpAndHumDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +18,14 @@ public class PaymentService {
     }
 
     // 결재자 목록 조회
-    public List<EmployeeAndEctJoinDTO> findAllApprovers() {
+    public List<DepAndEmpAndHumDTO> findAllApprovers() {
         return paymentMapper.findAllApprover();
     }
 
 
     // 결재선 추가 처리
-    public boolean addApprover(EmployeeAndEctJoinDTO employeeAndEctJoinDTO) {
-        int result = paymentMapper.inserApprover(employeeAndEctJoinDTO);
+    public boolean addApprover(DepAndEmpAndHumDTO depAndEmpAndHumDTO) {
+        int result = paymentMapper.inserApprover(depAndEmpAndHumDTO);
         return result > 0; // 성공 여부 반환
     }
 
