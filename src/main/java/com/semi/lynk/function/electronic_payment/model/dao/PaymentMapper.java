70cfe3c6cd1c0e38d4db07fe18f1b5b90a3ab15a
rlaskarb <rlaskarb20@gmail.com>
2024-12-13 13:15:26 +0900

    12/13 일반품의서 작성중

--- src/main/java/com/semi/lynk/function/electronic_payment/model/dao/PaymentMapper.java
+++ src/main/java/com/semi/lynk/function/electronic_payment/model/dao/PaymentMapper.java
@@ -1,18 +1,16 @@
 package com.semi.lynk.function.electronic_payment.model.dao;
 
 import com.semi.lynk.function.electronic_payment.model.dto.ApproveDTO;
-import com.semi.lynk.function.electronic_payment.model.dto.AuthorizationAndDepartmentAndEmployeeAndHumanResourceDTO;
-import com.semi.lynk.function.electronic_payment.model.dto.EmployeeDTO;
+import com.semi.lynk.function.electronic_payment.model.dto.EmployeeAndEctJoinDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+
 @Mapper
 public interface PaymentMapper {
 
-
-    List<AuthorizationAndDepartmentAndEmployeeAndHumanResourceDTO> findAllEmployees();
-
-    int insertApprover(ApproveDTO approveDTO);
+    List<EmployeeAndEctJoinDTO> findAllApprover();
 
 
+    int inserApprover(EmployeeAndEctJoinDTO employeeAndEctJoinDTO);
 }
--- src/main/java/com/semi/lynk/function/electronic_payment/service/PaymentService.java
+++ src/main/java/com/semi/lynk/function/electronic_payment/service/PaymentService.java
@@ -2,12 +2,10 @@ package com.semi.lynk.function.electronic_payment.service;
 
 import com.semi.lynk.function.electronic_payment.model.dao.PaymentMapper;
 import com.semi.lynk.function.electronic_payment.model.dto.ApproveDTO;
-import com.semi.lynk.function.electronic_payment.model.dto.AuthorizationAndDepartmentAndEmployeeAndHumanResourceDTO;
-import com.semi.lynk.function.electronic_payment.model.dto.DraftDTO;
-import com.semi.lynk.function.electronic_payment.model.dto.EmployeeDTO;
+import com.semi.lynk.function.electronic_payment.model.dto.EmployeeAndEctJoinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Service
@@ -20,18 +18,19 @@ public class PaymentService {
         this.paymentMapper = paymentMapper;
     }
 
-
-    public
-    List<AuthorizationAndDepartmentAndEmployeeAndHumanResourceDTO> getAllEmployees() {
-        return paymentMapper.findAllEmployees();
+    // 결재자 목록 조회
+    public List<EmployeeAndEctJoinDTO> findAllApprovers() {
+        return paymentMapper.findAllApprover();
     }
 
 
     // 결재선 추가 처리
-    public boolean addApprover(ApproveDTO approveDTO) {
-        int result = paymentMapper.insertApprover(approveDTO);
+    public boolean addApprover(EmployeeAndEctJoinDTO employeeAndEctJoinDTO) {
+        int result = paymentMapper.inserApprover(employeeAndEctJoinDTO);
         return result > 0; // 성공 여부 반환
     }
+
+
 }
 
 
