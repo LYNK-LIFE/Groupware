713ad713de98161818c41afbe92746421fa7b096
rlaskarb <rlaskarb20@gmail.com>
2024-12-18 18:21:55 +0900

    관리자 페이지 데이터 불러오기 및 구성 설정

--- src/main/java/com/semi/lynk/function/management/controller/managedController.java
+++ src/main/java/com/semi/lynk/function/management/controller/managedController.java
@@ -3,22 +3,33 @@ package com.semi.lynk.function.management.controller;
 import com.semi.lynk.function.management.service.ManagedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/management/*")
 public class managedController {
 
-    //****************************************************************
-    // 관리자 페이지 진입
-    //****************************************************************
     @Autowired
     private ManagedService managedService;
 
+    // 관리자 페이지 진입
     @GetMapping("activeAccountList")
-    public String activeAccountListPage() {
-        return "/function/management/activeAccountList";
+    public String activeAccountListPage(Model model) {
+        // 활성화 계정 개수 카운트와 정지 계정 카운트값
+        Map<String, Object> counts = managedService.getMemberStatusCounts();
+        model.addAttribute("activeAcc", counts.get("activeAcc"));
+        model.addAttribute("suspendedAcc", counts.get("suspendedAcc"));
+
+        // 활성화 계정 목록 조회
+        List<Map<String, Object>> activeAccs = managedService.getActiveEmployee();
+        model.addAttribute("activeAccs", activeAccs);
+
+        return "function/management/activeAccountList";
     }
 
 }
---
+++ src/main/java/com/semi/lynk/function/management/model/dto/AccountDTO.java
@@ -0,0 +1,20 @@
+package com.semi.lynk.function.management.model.dto;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AccountDTO {
+    private String empID;
+    private String empName;
+    private String deptName;
+    private String position;
+    private String email;
+    private Date loginTime;
+    private int memberStatus;
+}
