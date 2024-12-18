ab18c35c0ce7fd3646a03a1ab6bef083ffb2ea58
rlaskarb <rlaskarb20@gmail.com>
2024-12-19 01:28:22 +0900

    12/19 기안문서 저장하기가 어렵네요 ㅠ

--- src/main/java/com/semi/lynk/function/electronic_payment/service/PaymentService.java
+++ src/main/java/com/semi/lynk/function/electronic_payment/service/PaymentService.java
@@ -2,13 +2,13 @@ package com.semi.lynk.function.electronic_payment.service;
 
 import com.semi.lynk.function.electronic_payment.model.dao.PaymentMapper;
 import com.semi.lynk.function.electronic_payment.model.dto.DepAndEmpAndHumDTO;
-import com.semi.lynk.function.electronic_payment.model.dto.FullDraftDTO;
+import com.semi.lynk.function.electronic_payment.model.dto.DepartmentDTO;
+import com.semi.lynk.function.electronic_payment.model.dto.DraftDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class PaymentService {
@@ -20,25 +20,22 @@ public class PaymentService {
         this.paymentMapper = paymentMapper;
     }
 
+
     // 결재자 목록 조회
     public List<DepAndEmpAndHumDTO> findAllApprovers() {
         return paymentMapper.findAllApprover();
     }
 
 
-    // 결재선 추가 처리
-    public boolean addApprover(DepAndEmpAndHumDTO depAndEmpAndHumDTO) {
-        int result = paymentMapper.insertApprover(depAndEmpAndHumDTO);
-        return result > 0; // 성공 여부 반환
-    }
-
     @Transactional
-    public void saveDraft(FullDraftDTO fullDraftDTO) {
-        System.out.println("Saving DTO = " + fullDraftDTO);
-        paymentMapper.insertDraft(fullDraftDTO);
+    public void saveDraft(DraftDTO draftDTO) {
+        paymentMapper.insertDraft(draftDTO);
     }
 
 
+    public List<DepartmentDTO> findAllDepartments() {
+        return paymentMapper.findAllDepartment();
+    }
 }
 
 
