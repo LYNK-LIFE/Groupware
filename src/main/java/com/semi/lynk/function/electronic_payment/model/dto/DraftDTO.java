82bef5a190dbb9e0408ca4f90b6f58fab451e23c
rlaskarb <rlaskarb20@gmail.com>
2024-12-18 11:13:43 +0900

    12/18  //  pull 당기기 위한 커밋

--- src/main/java/com/semi/lynk/function/electronic_payment/model/dto/DraftDTO.java
+++ src/main/java/com/semi/lynk/function/electronic_payment/model/dto/DraftDTO.java
@@ -2,13 +2,15 @@ package com.semi.lynk.function.electronic_payment.model.dto;
 
 import lombok.*;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class DraftDTO {
-    private int draftDate; // 작성일자
+    private Date draftDate; // 작성일자
     private int draftStatus; // 결재상태
     private int draftNo; // 기안번호
     private String draftMemo; // 비고
