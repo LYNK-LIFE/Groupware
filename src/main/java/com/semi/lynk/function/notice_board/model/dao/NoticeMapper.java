adfec039ea4ed9bdf6ede34a7629a67f585d042e
Apexape <apexape84@gmail.com>
2024-12-23 21:52:50 +0900

    공지사항 삭제버튼 추가, 관리자페이지 추가, 페이지별로 DB에서 data가져오게 수정, 검색기능 추가

--- src/main/java/com/semi/lynk/function/notice_board/model/dao/NoticeMapper.java
+++ src/main/java/com/semi/lynk/function/notice_board/model/dao/NoticeMapper.java
@@ -8,9 +8,32 @@ import java.util.List;
 @Mapper
 public interface NoticeMapper {
     void insertNotice(NoticeDTO noticeDTO);
-    List<NoticeDTO> getAllNotices();
-    NoticeDTO selectNoticeById(Long noticeNo);
+    NoticeDTO selectNoticeByNNO(Long noticeNo);
     void updateNotice(Long noticeNo);
     void deleteNotice(Long noticeNo);
     void updateViewCnt(Long noticeNo);
+
+    List<NoticeDTO> getPinedNotices(int pageNo, int count);
+
+    List<NoticeDTO> getNormalNotices(int pageNo, int count);
+
+    List<NoticeDTO> getAllNotices(int pageNo, int count);
+
+    int getPinedCount();
+
+    int getNormalCount();
+
+    int getAllCount();
+
+    List<NoticeDTO> searchByNoticeNo(String keyword, int page, int count);
+
+    int getSearchNoCount(int keyword);
+
+    List<NoticeDTO> searchByNoticeTitle(String keyword, int page, int count);
+
+    int getSearchTitleCount(String keyword);
+
+    List<NoticeDTO> searchByNoticeEmp(String keyword, int page, int count);
+
+    int getSearchEmpCount(String keyword);
 }
--- src/main/java/com/semi/lynk/function/notice_board/service/NoticeServiceImpl.java
+++ src/main/java/com/semi/lynk/function/notice_board/service/NoticeServiceImpl.java
@@ -17,27 +17,79 @@ public class NoticeServiceImpl implements NoticeService {
     private NoticeMapper noticeMapper;
 
     @Override
-    public Page<NoticeDTO> getAllNoticesPaged(int page, int size) {
-        List<NoticeDTO> allNotices = noticeMapper.getAllNotices(); // 비효율적이다... 창피함
+    public Page<NoticeDTO> getNoticesPaged(int page, int size) {
+        int pinedCount = noticeMapper.getPinedCount(); // 고정목록글 갯수
+        int normalCount = noticeMapper.getNormalCount(); // 고정이 아닌글 갯수
+        int start = page * size; // 해당페이지의 시작글번호
 
+        if(pinedCount>start) {
+            int end = Math.min(pinedCount, start+size)-start;
+            List<NoticeDTO> allNotices = noticeMapper.getPinedNotices(start, end);
+            if(end!=12) {
+                List<NoticeDTO> normalNotices = noticeMapper.getNormalNotices(0, 12 - (end % 12));
+                allNotices.addAll(normalNotices);
+            }
+            return new PageImpl<>(allNotices, PageRequest.of(page, size), pinedCount+normalCount);
+        } else {
+            System.out.println(start-pinedCount);
+            List<NoticeDTO> allNotices = noticeMapper.getNormalNotices(start-pinedCount, 12);
+            return new PageImpl<>(allNotices, PageRequest.of(page, size), pinedCount+normalCount);
+        }
         // 고정글을 앞에 넣는 작업
         // count(notice_hide==2)가 12 초과이면 
         // offset으로 12개씩 받아오게 한다면, notice_hide == 2인 목록의 전체 카운트를 받아와서 12 이하면 출력 후
         // notic_hide == 0인 글을 12-count(notice_hide==2)갯수만큼 출력
-        allNotices.sort((a, b) -> {
-            if (a.getNoticeHide() == 2 && b.getNoticeHide() != 2) {
-                return -1;
-            } else if (a.getNoticeHide() != 2 && b.getNoticeHide() == 2) {
-                return 1;
-            }
-            return 0;
-        });
+    }
 
-        // Perform manual pagination
+    @Override
+    public Page<NoticeDTO> getAllNotices(int page, int size) {
+        int pinedCount = noticeMapper.getPinedCount(); // 고정목록글 갯수
+        int allCount = noticeMapper.getAllCount(); // 고정이 아닌글 갯수
+        int start = page * size; // 해당페이지의 시작글번호
+
+        if(pinedCount>start) {
+            int end = Math.min(pinedCount, start+size)-start;
+            List<NoticeDTO> allNotices = noticeMapper.getPinedNotices(start, end);
+            if(end!=12) {
+                List<NoticeDTO> otherNotices = noticeMapper.getAllNotices(0, 12 - (end % 12));
+                allNotices.addAll(otherNotices);
+            }
+            return new PageImpl<>(allNotices, PageRequest.of(page, size), allCount);
+        } else {
+            System.out.println(start-pinedCount);
+            List<NoticeDTO> allNotices = noticeMapper.getAllNotices(start-pinedCount, 12);
+            return new PageImpl<>(allNotices, PageRequest.of(page, size), allCount);
+        }
+    }
+
+    @Override
+    public Page<NoticeDTO> searchNotices(String searchType, String keyword, int page, int size){
         int start = page * size;
-        int end = Math.min((start + size), allNotices.size()); // allnotice의 사이즈를 측정하지 말고, 쿼리의
-        List<NoticeDTO> pagedNotices = allNotices.subList(start, end);
-        return new PageImpl<>(pagedNotices, PageRequest.of(page, size), allNotices.size());
+        String number = "공지번호";
+        String title = "제목";
+
+        System.out.println("여긴 서비스+++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        if(searchType.equals(number)) {
+            System.out.println("여긴 if문=======================================================");
+            int searchNoCount = noticeMapper.getSearchNoCount(Integer.parseInt(keyword));
+            System.out.println("searchNoCount = " + searchNoCount);
+            int end = Math.min(searchNoCount, start+size)-start;
+            System.out.println("end = " + end);
+            List<NoticeDTO> allNotices = noticeMapper.searchByNoticeNo(keyword, page, end);
+            return new PageImpl<>(allNotices, PageRequest.of(page, size), searchNoCount);
+        }else if(title.equals(searchType)){
+            int searchTitleCount = noticeMapper.getSearchTitleCount(keyword);
+            int end = Math.min(searchTitleCount, start+size)-start;
+            List<NoticeDTO> allNotices = noticeMapper.searchByNoticeTitle(keyword, page, end);
+            return new PageImpl<>(allNotices, PageRequest.of(page, size), searchTitleCount);
+        }else {
+            int searchEmpCount = noticeMapper.getSearchEmpCount(keyword);
+            int end = Math.min(searchEmpCount, start+size)-start;
+            List<NoticeDTO> allNotices = noticeMapper.searchByNoticeEmp(keyword, page, end);
+            return new PageImpl<>(allNotices, PageRequest.of(page, size), searchEmpCount);
+        }
+
     }
 
 
@@ -47,16 +99,10 @@ public class NoticeServiceImpl implements NoticeService {
         noticeMapper.insertNotice(noticeDTO);
     }
 
-    @Override
-    public List<NoticeDTO> getAllNotices() {
-        List<NoticeDTO> noticeDTOList = (List<NoticeDTO>) noticeMapper.getAllNotices();
-        // 무식하게 다 받아와서 조건이 없음. 추후 페이지 수를 통해 몇번째 글부터 받아오면 되는지 넘겨서 12개씩 받아오게 할 것.
-        return noticeDTOList;
-    }
 
     @Override
-    public NoticeDTO getNoticeById(Long noticeNo) {
-        return noticeMapper.selectNoticeById(noticeNo);
+    public NoticeDTO getNoticeByNNO(Long noticeNo) {
+        return noticeMapper.selectNoticeByNNO(noticeNo);
     }
 
     @Override
@@ -66,7 +112,7 @@ public class NoticeServiceImpl implements NoticeService {
 
     @Override
     public void deleteNotice(Long noticeNo) {
-        noticeMapper.deleteNotice(noticeNo);
+        noticeMapper.updateNotice(noticeNo);
     }
 
     @Override
